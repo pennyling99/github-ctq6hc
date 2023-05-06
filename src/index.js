8fccd2244cff1a742d1f5488a03d0726071f93ae
pennyling99 <38998246+pennyling99@users.noreply.github.com>
2023-05-06 16:26:47 +0800

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import _ from 'lodash';
 import Vue from 'vue';
 import App from './App.vue';
 
-const vm = new Vue({
-  el: '#app',
-  template: `<div id="app>{{message}}</div>`,
-  data: {
-    message: 'Hello Vue',
-  },
-  render: (h) => h(App),
-});
+document.getElementById('app').innerHTML = 'Hello, Vue';
